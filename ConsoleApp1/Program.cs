53abdc1b8c1cda02fcf760f0246f2fc39011d2b5
DinaRahma <dinarahmaa354@gmail.com>
2019-04-02 12:02:17 +0700

    ConsoleApp1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // deklrasai variabel
+            int hasil;
+
+            // membuat objek dari class Calculator
+            Class_calculator calculator = new Class_calculator();
+
+            // panggil method Penjumlah
+            hasil = calculator.Penjumlah(10, 2);
+            CetakHasil("Hasil Penjumlah: " + hasil);
+
+            // panggil method Penjumlah dg 3 parameter (overload method)
+            hasil = calculator.Penjumlah(10, 2, 3);
+            CetakHasil("Hasil Penjumlah overload: " + hasil);
+
+            // panggil method Pengurangan 
+            hasil = calculator.Pengurangan(7, 2);
+            CetakHasil("Hasil Pengurangan: " + hasil);
+
+            // panggil static method, langsung dari nama classnya
+            hasil = Class_calculator.Perkalian(5, 2);
+            CetakHasil("Hasil Perkalian: " + hasil);
+
+            Console.ReadKey();
+
+        }
+        static void CetakHasil(string hasil)
+        {
+            Console.WriteLine(hasil);
+        }
+    }
+}
